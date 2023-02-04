d4f2ceba35c3c1545ce5502decf7b447e9d237c9
MOSYSNIPE\mrah1 <m.rah1372@gmail.com>
2023-02-04 18:15:09 +0330

    Example19: If statement

---
+++ Introduction/example19.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash 
+
+# If statement
+
+A=100
+if [ $A -gt 100 ]; then 
+	echo "a is greater than 100" 
+	
+elif [ $A -eq 100 ];; then 
+	echo "a is equal 100" 
+	
+else
+	echo "a is less than 100" 
+fi 
